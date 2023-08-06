1be0015afc974ca3bb99b0118f382b12d2ef86fe
Sahana B <95917958+sahana0728@users.noreply.github.com>
2023-08-06 17:06:26 +0530

    goo

--- server.py
+++ server.py
@@ -1,35 +1,34 @@
-from flask import Flask,request,jsonify
-
+from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return 'hello world!',200
+    return 'hello world!', 200
+   
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    data=request.get_jsonify()
+    data = request.get_json()
     if 'first' not in data or 'second' not in data:
-        return jsonify('invalid '),400
-    first=data['first']
-    second=data['second']
-    result=first+second
-    return jsonify(result=result),200
-    
+        return jsonify(error='Invalid request. Both "first" and "second" numbers are required.'), 400
+
+    first = data['first']
+    second = data['second']
+    result = first + second
+    return jsonify(result=result), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    data=request.get_jsonify()
+    data = request.get_json()
     if 'first' not in data or 'second' not in data:
-        return jsonify('invalid '),400
-    first=data['first']
-    second=data['second']
-    result=first-second
-    return jsonify(result=result),200
-    
+        return jsonify(error='Invalid request. Both "first" and "second" numbers are required.'), 400
+
+    first = data['first']
+    second = data['second']
+    result = first - second
+    return jsonify(result=result), 200
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
